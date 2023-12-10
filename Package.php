81ec29217a3d64f271907a8dbf10aae3710de1c1
Teyir <60007549+Teyir@users.noreply.github.com>
2023-12-10 17:36:19 +0100

    [IMPROVES] Use new Package class

---
+++ Package.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace CMW\Package\Calendar;
+
+use CMW\Manager\Package\IPackageConfig;
+use CMW\Manager\Package\PackageMenuType;
+use CMW\Manager\Package\PackageSubMenuType;
+
+class Package implements IPackageConfig
+{
+    public function name(): string
+    {
+        return "Calendar";
+    }
+
+    public function version(): string
+    {
+        return "0.0.1";
+    }
+
+    public function authors(): array
+    {
+        return ["Zomb"];
+    }
+
+    public function isGame(): bool
+    {
+        return false;
+    }
+
+    public function isCore(): bool
+    {
+        return false;
+    }
+
+    public function menus(): ?array
+    {
+        return [
+            new PackageMenuType(
+                lang: "fr",
+                icon: "fas fa-calendar-days",
+                title: "Calendrier",
+                url: "calendar/manage",
+                permission: "calendar.show",
+                subMenus: []
+            ),
+            new PackageMenuType(
+                lang: "en",
+                icon: "fas fa-calendar-days",
+                title: "Calendar",
+                url: "calendar/manage",
+                permission: "calendar.show",
+                subMenus: []
+            ),
+        ];
+    }
+}
