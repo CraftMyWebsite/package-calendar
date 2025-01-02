b6c8b123f82c2603f986089f67410b5a5acb69ab
Léo <69589034+Zomblard@users.noreply.github.com>
2025-01-02 14:55:03 +0100

    [IMPROVE] default public views

--- Public/main.view.php
+++ Public/main.view.php
@@ -5,11 +5,6 @@ use CMW\Utils\Website;
 Website::setTitle('Calendrier');
 Website::setDescription('Découvrez nos futur événements');
 ?>
-<?php if (\CMW\Controller\Users\UsersController::isAdminLogged()): ?>
-    <div style="background-color: orange; padding: 6px; margin-bottom: 10px">
-        <span>Votre thème ne gère pas cette page !</span>
-        <br>
-        <small>Seuls les administrateurs voient ce message !</small>
-    </div>
-<?php endif;?>
+<section style="width: 70%;padding-bottom: 6rem;margin: 1rem auto auto;">
 <div id='calendar'></div>
+</section>
